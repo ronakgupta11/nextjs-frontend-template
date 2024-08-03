db1c88dc52d7a1754ad37676a991bb375b57dbe2
Ronak Gupta <93486131+ronakgupta11@users.noreply.github.com>
2024-08-03 14:45:24 +0530

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  daisyui: {
+    themes: ["cupcake", "dark", "bumblebee","emerald"],
+  },
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -14,5 +17,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
